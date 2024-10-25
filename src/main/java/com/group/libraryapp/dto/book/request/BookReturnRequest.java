f533bccb54f3671268ae801b05e6ff5d04786d5b
kjwjwjw <tbs03112@naver.com>
2024-10-25 19:30:46 +0900

    Book Test 부분

--- src/main/java/com/group/libraryapp/dto/book/request/BookReturnRequest.java
+++ src/main/java/com/group/libraryapp/dto/book/request/BookReturnRequest.java
@@ -13,4 +13,9 @@ public class BookReturnRequest {
     return bookName;
   }
 
+
+  public BookReturnRequest(String userName, String bookName) {
+    this.userName = userName;
+    this.bookName = bookName;
+  }
 }
